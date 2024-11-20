26f0a529efb403e92a13696406c3952168ac9c6c
Okuto Morikawa <107906773+o-morikawa@users.noreply.github.com>
2024-11-20 20:37:30 +0900

    Update om_sample_winding.jl

--- test/om_sample_winding.jl
+++ test/om_sample_winding.jl
@@ -6,34 +6,58 @@ using Gaugefields
 using LinearAlgebra
 using Wilsonloop
 
+using Plots
+
 function UN_test_3D(NX,NY,NT,NC,β)
 
     Dim = 3
 
-    #Random.seed!(123)
-    t0 = Dates.DateTime(2024,1,1,16,10,7)
-    t  = Dates.now()
-    Random.seed!(Dates.value(t-t0))
+    n = 3
 
-    U = Initialize_3D_UN_Gaugefields(
-        NC,NX,NY,NT,
-        condition = "hot",
-        randomnumber="Random"
-    )
-    println(typeof(U))
+    eps = 0.01
+    flow_number = 1000
+    step = 10
+    
+    w = zeros(Float64, n, Int(flow_number/step))
 
-    temps = Temporalfields(U, num=9)
-    println(typeof(temps))
+    for i = 1:n
 
-    g = Gradientflow_3D(U, eps=0.001)
-    flownumber = 1000
-    for iflow = 1:flownumber
-        flow!(U, g)
-        if iflow%100==0
-            W = winding_UN_3D(U,temps)
-            println(W)
+        #Random.seed!(123)
+        t0 = Dates.DateTime(2024,1,1,16,10,7)
+        t  = Dates.now()
+        Random.seed!(Dates.value(t-t0))
+
+        U = Initialize_3D_UN_Gaugefields(
+            NC,NX,NY,NT,
+            condition = "hot",
+            randomnumber="Random"
+        )
+        println(typeof(U))
+
+        temps = Temporalfields(U, num=9)
+        println(typeof(temps))
+
+        g = Gradientflow_3D(U, eps=eps)
+        flownumber = flow_number
+        j = 0
+        for iflow = 1:flownumber
+            flow!(U, g)
+            if iflow%step==0
+                j += 1
+                W = winding_UN_3D(U,temps)
+                w[i,j] = W
+            end
         end
     end
+
+    println(w)
+    
+    flow = (eps*step):(eps*step):(eps*flow_number)
+    plt = plot(flow, w[1,:])
+    for i = 2:n
+        plot!(plt, flow, w[i,:])
+    end
+    savefig("winding.png")
 
 end
 
