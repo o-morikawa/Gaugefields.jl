0a04b2e13abbed761b434a3687da9c6c1a248e83
Okuto Morikawa <107906773+o-morikawa@users.noreply.github.com>
2025-06-02 14:03:09 +0900

    Add files via upload

--- src/AbstractGaugefields.jl
+++ src/AbstractGaugefields.jl
@@ -1968,11 +1968,11 @@ function sweepaway_4D_Bplaquettes_origin!(
             substitute_U!(Unew, uout)
             #Ushift = shift_U(Unew, origin)
             substitute_U!(Bshift12new, Bshift12)
-            Bshift12 = shift_U(Bshift12new, origin)
+            Bshift12 = shift_U(Bshift12new, origin_minus)
             substitute_U!(Bshift13new, Bshift13)
-            Bshift13 = shift_U(Bshift13new, origin)
+            Bshift13 = shift_U(Bshift13new, origin_minus)
             substitute_U!(Bshift14new, Bshift14)
-            Bshift14 = shift_U(Bshift14new, origin)
+            Bshift14 = shift_U(Bshift14new, origin_minus)
 
         end
 
@@ -1998,9 +1998,9 @@ function sweepaway_4D_Bplaquettes_origin!(
             substitute_U!(Unew, uout)
             #Ushift = shift_U(Unew, origin)
             substitute_U!(Bshift13new, Bshift13)
-            Bshift13 = shift_U(Bshift13new, origin)
+            Bshift13 = shift_U(Bshift13new, origin_minus)
             substitute_U!(Bshift14new, Bshift14)
-            Bshift14 = shift_U(Bshift14new, origin)
+            Bshift14 = shift_U(Bshift14new, origin_minus)
 
         end
 
@@ -2022,7 +2022,7 @@ function sweepaway_4D_Bplaquettes_origin!(
             substitute_U!(Unew, uout)
             #Ushift = shift_U(Unew, origin)
             substitute_U!(Bshift14new, Bshift14)
-            Bshift14 = shift_U(Bshift14new, origin)
+            Bshift14 = shift_U(Bshift14new, origin_minus)
 
         end
     elseif direction == 2
@@ -2090,9 +2090,9 @@ function sweepaway_4D_Bplaquettes_origin!(
             substitute_U!(Unew, uout)
             #Ushift = shift_U(Unew, origin)
             substitute_U!(Bshift23new, Bshift23)
-            Bshift23 = shift_U(Bshift23new, origin)
+            Bshift23 = shift_U(Bshift23new, origin_minus)
             substitute_U!(Bshift24new, Bshift24)
-            Bshift24 = shift_U(Bshift24new, origin)
+            Bshift24 = shift_U(Bshift24new, origin_minus)
 
         end
 
@@ -2114,7 +2114,7 @@ function sweepaway_4D_Bplaquettes_origin!(
             substitute_U!(Unew, uout)
             #Ushift = shift_U(Unew, origin)
             substitute_U!(Bshift24new, Bshift24)
-            Bshift24 = shift_U(Bshift24new, origin)
+            Bshift24 = shift_U(Bshift24new, origin_minus)
         end
     elseif direction == 3
         Bshift34 = temps[7]
@@ -2175,7 +2175,7 @@ function sweepaway_4D_Bplaquettes_origin!(
             substitute_U!(Unew, uout)
             #Ushift = shift_U(Unew, origin)
             substitute_U!(Bshift34new, Bshift34)
-            Bshift34 = shift_U(Bshift34new, origin)
+            Bshift34 = shift_U(Bshift34new, origin_minus)
 
         end
     else
